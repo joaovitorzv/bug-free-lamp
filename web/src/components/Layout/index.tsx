7575aadba3a7a2a8ed96f0e91b15a54bdb2ff0f6
João V. Veras <joaovitorzv@outlook.com>
2022-03-27 05:51:29 -0300

    feat: signout action

--- web/src/components/Layout/index.tsx
+++ web/src/components/Layout/index.tsx
@@ -1,13 +1,17 @@
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { signout } from "../../actions/sessionSlice";
 
 function Layout() {
+  const dispatch = useDispatch();
+
   return (
     <div>
       <header
         style={{ padding: "10px", backgroundColor: "black", color: "white" }}
       >
         <h2>that's definitely the header!</h2>
-        <button />
+        <button onClick={() => dispatch(signout())}>signout</button>
       </header>
       <Outlet />
     </div>
